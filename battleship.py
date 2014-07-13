7a617008144e9ab7eb0c95842261972a92b085b1
Simon Fiddaman <simon@fiddaman.com.au>
2014-07-13 20:45:22 +0000

    Add helper numbers, use 1-based row,col numbering; mark sunk ship; mark failed ship on last turn

--- battleship.py
+++ battleship.py
@@ -8,33 +8,40 @@ for x in range(5):
     board.append(["O"] * 5)
 
 def print_board(board):
+    print "+ 1 2 3 4 5 +"
+    colnote = 1
     for row in board:
-        print " ".join(row)
+        print colnote,
+	print " ".join(row),
+	print colnote
+	colnote += 1
+    print "+ 1 2 3 4 5 +"
 
 print "Let's play Battleship!"
 print_board(board)
 
 def random_row(board):
-    return randint(0, len(board) - 1)
+    return randint(0, len(board) -1 )
 
 def random_col(board):
-    return randint(0, len(board[0]) - 1)
+    return randint(0, len(board[0]) -1)
 
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-print ship_row
-print ship_col
+#print int(ship_row) + 1
+#print int(ship_col) + 1
 
 # Everything from here on should go in your for loop!
 # Be sure to indent four spaces!
 for turn in range(4):
     print "Turn", turn +1
-    guess_row = int(raw_input("Guess Row:"))
-    guess_col = int(raw_input("Guess Col:"))
+    guess_row = int(raw_input("Guess Row:")) -1
+    guess_col = int(raw_input("Guess Col:")) -1
 
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
+	board[guess_row][guess_col] = "^"
         break
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
@@ -44,6 +51,7 @@ for turn in range(4):
         else:
             if turn == 3:
                 print "Game Over"
+		board[guess_row][guess_col] = "X"
             else:
                 print "You missed my battleship!"
                 board[guess_row][guess_col] = "X"
